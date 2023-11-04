d0b4433c529c28f92c33aa9123d003fe375337e2
ALIMOSA <am3468000@gmailcom>
2023-11-04 14:35:16 +0200

    restructuring for DRY S and solve minus

--- ConsoleCalculator.java
+++ ConsoleCalculator.java
@@ -2,6 +2,14 @@ package com.mycompany.consolecalculator;
 
 import java.util.Scanner;
 import java.util.ArrayList;
+
+
+// TODO 
+// 1. Handle parentheses
+// 2. 1/2 integer to double
+// 3. restructuring
+
+
 public class ConsoleCalculator {
     
     public static void main(String[] args) {
@@ -17,8 +25,8 @@ public class ConsoleCalculator {
             }
             
             try {
-                ArrayList<String> array = tokenize(input);
-                int result = solve(array);
+                ArrayList<String> tokens = tokenize(input);
+                int result = solve(tokens);
                 System.out.println(result);
             }
             catch (Exception e) {
@@ -27,11 +35,11 @@ public class ConsoleCalculator {
         }
     }    
     
-    public static boolean isDigit(char c){
+    public static boolean isDigit(char c) {
         return (c >= '0' && c <= '9');
     }
     
-    public static boolean isOperator(char c){
+    public static boolean isOperator(char c) {
         return (
             c == '+'
             || c == '-'
@@ -53,11 +61,6 @@ public class ConsoleCalculator {
                 
             } 
             else if (isOperator(c)){
-                if(c == '-' && (i == 0 || isOperator(equation.charAt(i - 1)))){
-                    currentNumber += c;
-                    continue;
-                }
- 
                 if(!currentNumber.isEmpty()){
                     tokens.add(currentNumber);
                     currentNumber = "";
@@ -90,55 +93,69 @@ public class ConsoleCalculator {
             throw new Exception("No input.");
         }
 
-        int result = 0;
         try {
-            
-       
-            for (int i = 1; i < tokens.size(); i = i + 2){
-                switch (tokens.get(i)) {
-                    case "*" -> {
-                        result = Integer.parseInt(tokens.get(i-1)) * Integer.parseInt(tokens.get(i+1));
-                        tokens.remove(i+1);
-                        tokens.remove(i);
-                        tokens.set(i-1, Integer.toString(result));
-                        i = i - 2;
-                    }
-                    case "/" -> {
-                        result = Integer.parseInt(tokens.get(i-1)) / Integer.parseInt(tokens.get(i+1));
-                        tokens.remove(i+1);
-                        tokens.remove(i);
-                        tokens.set(i-1, Integer.toString(result));
-                        i = i - 2;
-                    }
-                    default -> {}
-                }
-            }
-            
-            for (int i = 1; i < tokens.size(); i = i + 2){
-                switch (tokens.get(i)) {
-                    case "+" -> {
-                        result = Integer.parseInt(tokens.get(i-1)) + Integer.parseInt(tokens.get(i+1));
-                        tokens.remove(i+1);
-                        tokens.remove(i);
-                        tokens.set(i-1, Integer.toString(result));
-                        i = i - 2;
-                    }
-                    case "-" -> {
-                        result = Integer.parseInt(tokens.get(i-1)) - Integer.parseInt(tokens.get(i+1));
-                        tokens.remove(i+1);
-                        tokens.remove(i);
-                        tokens.set(i-1, Integer.toString(result));
-                        i = i - 2;
-                    }
-                    
-                    default -> {}
-                }
-            }
+            solveForminus(tokens);
+            solveForMultDiv(tokens);
+            solveForAddSub(tokens);
         } 
         catch (NumberFormatException e){ 
             throw new Exception ("Bad Input.");
         }
         
         return Integer.parseInt(tokens.get(0));
+    }
+    
+    public static void solveForMultDiv(ArrayList<String> tokens) {
+        for (int i = 1; i < tokens.size(); i = i + 2){
+            int left = Integer.parseInt(tokens.get(i-1));
+            int right = Integer.parseInt(tokens.get(i+1));
+            
+            switch (tokens.get(i)) {
+                case "*" -> {
+                    reduce(tokens, i - 1, 3, Integer.toString(left * right));
+                    i = i - 2;
+                }
+                case "/" -> {
+                    reduce(tokens, i - 1, 3, Integer.toString(left / right));
+                    i = i - 2;
+                }
+            }
+        }
+    }
+    
+    public static void solveForminus(ArrayList<String> tokens) {
+        for (int i = 0; i < tokens.size(); i++) {
+            if (tokens.get(i).equals("-") 
+                && (i == 0 || isOperator(tokens.get(i - 1).charAt(0)))) {
+                i -= reduce(tokens, i, 2, tokens.get(i) + tokens.get(i + 1));
+            } 
+        }
+    }
+    
+    public static void solveForAddSub(ArrayList<String> tokens) {
+        for (int i = 1; i < tokens.size(); i = i + 2){
+            int left = Integer.parseInt(tokens.get(i-1));
+            int right = Integer.parseInt(tokens.get(i+1));
+            
+            switch (tokens.get(i)) {
+                case "+" -> {
+                    i -= reduce(tokens, i - 1, 3, Integer.toString(left + right));
+                }
+                case "-" -> {
+                    i -= reduce(tokens, i - 1, 3, Integer.toString(left - right));
+                }
+            }
+        }
+    }
+    
+    // reduce reduces n places by 1 result (e.g. [1 + 2] -> [3])
+    public static int reduce(ArrayList<String> tokens, int position, int n, String replacement) {
+        tokens.set(position, replacement);
+        
+        for (int i = 0; i < n - 1; i++) {
+            tokens.remove(position + 1);
+        }
+        
+        return n - 1;
     }
 }
